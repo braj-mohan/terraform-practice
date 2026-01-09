394e946795e26e5cb36e14dcbbba06eb4678eb93
braj-mohan <32188248+braj-mohan@users.noreply.github.com>
2026-01-09 21:39:37 +0530

    Clean up variables.tf by removing unused code
    Removed unused resources and outputs from variables.tf

--- variables.tf
+++ variables.tf
@@ -1,21 +1,3 @@
-resource "local_file" "devops" {
-  filename = "/home/practicevm1/Day1/terraform-variables/devopstest.txt"
-  content  = var.content_map["content1"]
-}
-
-resource "local_file" "devops-var" {
-  filename = var.filename
-  content  = var.content_map["content2"]
-}
-
-output "devops_op_trainer" {
-  value = var.devops_op_trainer
-}
-
-output "aws_ec2_instance" {
-  value = var.aws_ec2_object
-}
-practicevm1@practicevm1:~/Day1/terraform-variables$ cat variables.tf
 variable "filename" {
   default = "/home/practicevm1/Day1/terraform-variables/devope_automated.txt"
 }
@@ -55,6 +37,3 @@ default = {
 }
 }
 
-output "tf_batch_students" {
-  value = var.no_of_students
-}
