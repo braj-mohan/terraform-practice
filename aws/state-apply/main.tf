b45ea567e2e16746b54ad25c6030d03ab78e7afe
braj-mohan <32188248+braj-mohan@users.noreply.github.com>
2026-01-14 14:01:00 +0530

    Add AWS provider and EC2 instance configuration

---
+++ aws/state-apply/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
+resource "aws_instance" "my_ec2_instance" {
+	count = 2
+	ami = "ami-02b8269d5e85954ef"
+	instance_type = "t3.micro"
+	tags = {
+		Name = "terraform-test-lock"
+}
+
+}
